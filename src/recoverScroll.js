fad98366e2b48c6877254bf0373ae6251a00bd57
华仔_biubiubiu <452216418@qq.com>
2017-11-13 01:20:57 -0600

    Update recoverScroll.js

--- src/recoverScroll.js
+++ src/recoverScroll.js
@@ -8,3 +8,19 @@ export default {
 		document.removeEventListener('touchmove', preventPageScroll, false);
 	}
 }
+
+(function (doc, win) {
+  var docEl = doc.documentElement,
+    resizeEvt = "orientationchange" in window ? "orientationchange" : "resize",
+    recalc = function () {
+      var clientWidth = docEl.clientWidth;
+      if (clientWidth >= 768) {
+        clientWidth = 768;
+      }
+      if (!clientWidth) return;
+      docEl.style.fontSize = 100 * (clientWidth / 375) + "px";
+    };
+  if (!doc.addEventListener) return;
+  win.addEventListener(resizeEvt, recalc, false);
+  doc.addEventListener("DOMContentLoaded", recalc, false);
+})(document, window);
