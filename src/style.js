ad869b6157a5ea510c15b3520d74b69c49e5d569
华仔_biubiubiu <452216418@qq.com>
2017-11-24 14:23:06 +0800

    Update style.js

--- src/style.js
+++ src/style.js
@@ -39,3 +39,54 @@ function Toast(text, time) {
         var timer2 = window.setInterval(fade_out, 100);
     }, time - 400);
 }
+
+/**
+时间格式化
+**/
+
+function Moment(date) {
+  var date = date ? new Date(date) : new Date();
+
+  this.Object.prototype.format = function(str) {
+    var that = this;
+    if (str == 'X') {
+      return Math.round(that.getTime() / 1000);
+    } else if (str == 'XX') {
+      return that.getTime();
+    } else {
+      var str = str ? str : 'YYYY-MM-DD hh:mm:ss';
+      function addZero(data) {
+        if (data < 10) {
+          return '0' + data;
+        } else {
+          return data;
+        }
+      }
+      var _YYYY = that.getFullYear();
+      var _MM = addZero(that.getMonth() + 1);
+      var _DD = addZero(that.getDate());
+      var _hh = addZero(that.getHours());
+      var _mm = addZero(that.getMinutes());
+      var _ss = addZero(that.getSeconds());
+      var _ww = that.getDay();
+      var arr1 = ['星期天', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
+      var arr2 = ['周日', '周一', '周二', '周三', '周四', '周五', '周六'];
+      var _WW = arr1[_ww];
+      _ww = arr2[_ww];
+      var str = str
+        .replace(/YYYY/g, _YYYY)
+        .replace(/MM/g, _MM)
+        .replace(/DD/g, _DD)
+        .replace(/WW/g, _WW)
+        .replace(/ww/g, _ww)
+        .replace(/DD/g, _DD)
+        .replace(/hh/g, _hh)
+        .replace(/mm/g, _mm)
+        .replace(/ss/g, _ss);
+
+      return str;
+    }
+  };
+  return date;
+}
+
